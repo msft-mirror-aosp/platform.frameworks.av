github583431183/framework_av

C2BqBuffer: defer GraphicBuffer cache clearing after stop()
Also enable vendors to control the deferring behavior using
int32 property: "debug.codec2.bqpool_defer_deallocate"
 0 : no deferred clear
 1 : deferred clear only on swcodec
 other values: deferred clear on all codec HAL

Bug: 322731059
Change-Id: I92cdb1bfcd7856646cb1baa659bf273344cd6e11

media/codec2/hal/hidl/1.0/utils/Component.cpp
@@ -521,7 +521,18 @@ Return<Status> Component::start() {
 
 Return<Status> Component::stop() {
     InputBufferManager::unregisterFrameData(mListener);
-    return static_cast<Status>(mComponent->stop());
+    Status status = static_cast<Status>(mComponent->stop());
+    {
+        std::lock_guard<std::mutex> lock(mBlockPoolsMutex);
+        for (auto it = mBlockPools.begin(); it != mBlockPools.end(); ++it) {
+            if (it->second->getAllocatorId() == C2PlatformAllocatorStore::BUFFERQUEUE) {
+                std::shared_ptr<C2BufferQueueBlockPool> bqPool =
+                        std::static_pointer_cast<C2BufferQueueBlockPool>(it->second);
+                bqPool->clearDeferred();
+            }
+        }
+    }
+    return status;
 }
 
 Return<Status> Component::reset() {

media/codec2/hal/hidl/1.1/utils/Component.cpp
@@ -527,7 +527,18 @@ Return<Status> Component::start() {
 
 Return<Status> Component::stop() {
     InputBufferManager::unregisterFrameData(mListener);
-    return static_cast<Status>(mComponent->stop());
+    Status status = static_cast<Status>(mComponent->stop());
+    {
+        std::lock_guard<std::mutex> lock(mBlockPoolsMutex);
+        for (auto it = mBlockPools.begin(); it != mBlockPools.end(); ++it) {
+            if (it->second->getAllocatorId() == C2PlatformAllocatorStore::BUFFERQUEUE) {
+                std::shared_ptr<C2BufferQueueBlockPool> bqPool =
+                        std::static_pointer_cast<C2BufferQueueBlockPool>(it->second);
+                bqPool->clearDeferred();
+            }
+        }
+    }
+    return status;
 }
 
 Return<Status> Component::reset() {

media/codec2/hal/hidl/1.2/utils/Component.cpp
@@ -524,6 +524,18 @@ Return<Status> Component::start() {
 Return<Status> Component::stop() {
     InputBufferManager::unregisterFrameData(mListener);
     return static_cast<Status>(mComponent->stop());
+    Status status = static_cast<Status>(mComponent->stop());
+    {
+        std::lock_guard<std::mutex> lock(mBlockPoolsMutex);
+        for (auto it = mBlockPools.begin(); it != mBlockPools.end(); ++it) {
+            if (it->second->getAllocatorId() == C2PlatformAllocatorStore::BUFFERQUEUE) {
+                std::shared_ptr<C2BufferQueueBlockPool> bqPool =
+                        std::static_pointer_cast<C2BufferQueueBlockPool>(it->second);
+                bqPool->clearDeferred();
+            }
+        }
+    }
+    return status;
 }
 
 Return<Status> Component::reset() {

msft-mirror-aosp/platform.frameworks.av

int32 property: "debug.codec2.bqpool_dealloc_after_stop"
 0         : no deferred clear
 any value : deferred clear(only on swcodec)
Bug: 328440882
(cherry picked from https://android-review.googlesource.com/q/commit:2e979ed0842148fe3d97c06ec7061361568c753b)
Merged-In: I92cdb1bfcd7856646cb1baa659bf273344cd6e11

media/codec2/hal/hidl/1.0/utils/Component.cpp
@@ -460,7 +460,18 @@ Return<Status> Component::start() {
 
 Return<Status> Component::stop() {
     InputBufferManager::unregisterFrameData(mListener);
-    return static_cast<Status>(mComponent->stop());
+    Status status = static_cast<Status>(mComponent->stop());
+    {
+        std::lock_guard<std::mutex> lock(mBlockPoolsMutex);
+        for (auto it = mBlockPools.begin(); it != mBlockPools.end(); ++it) {
+            if (it->second->getAllocatorId() == C2PlatformAllocatorStore::BUFFERQUEUE) {
+                std::shared_ptr<C2BufferQueueBlockPool> bqPool =
+                        std::static_pointer_cast<C2BufferQueueBlockPool>(it->second);
+                bqPool->clearDeferredBlocks();
+            }
+        }
+    }
+    return status;
 }
 
 Return<Status> Component::reset() {

media/codec2/hal/hidl/1.1/utils/Component.cpp
@@ -460,7 +460,18 @@ Return<Status> Component::start() {
 
 Return<Status> Component::stop() {
     InputBufferManager::unregisterFrameData(mListener);
-    return static_cast<Status>(mComponent->stop());
+    Status status = static_cast<Status>(mComponent->stop());
+    {
+        std::lock_guard<std::mutex> lock(mBlockPoolsMutex);
+        for (auto it = mBlockPools.begin(); it != mBlockPools.end(); ++it) {
+            if (it->second->getAllocatorId() == C2PlatformAllocatorStore::BUFFERQUEUE) {
+                std::shared_ptr<C2BufferQueueBlockPool> bqPool =
+                        std::static_pointer_cast<C2BufferQueueBlockPool>(it->second);
+                bqPool->clearDeferredBlocks();
+            }
+        }
+    }
+    return status;
 }
 
 Return<Status> Component::reset() {

media/codec2/hal/hidl/1.2/utils/Component.cpp
@@ -460,7 +460,18 @@ Return<Status> Component::start() {
 
 Return<Status> Component::stop() {
     InputBufferManager::unregisterFrameData(mListener);
-    return static_cast<Status>(mComponent->stop());
+    Status status = static_cast<Status>(mComponent->stop());
+    {
+        std::lock_guard<std::mutex> lock(mBlockPoolsMutex);
+        for (auto it = mBlockPools.begin(); it != mBlockPools.end(); ++it) {
+            if (it->second->getAllocatorId() == C2PlatformAllocatorStore::BUFFERQUEUE) {
+                std::shared_ptr<C2BufferQueueBlockPool> bqPool =
+                        std::static_pointer_cast<C2BufferQueueBlockPool>(it->second);
+                bqPool->clearDeferredBlocks();
+            }
+        }
+    }
+    return status;
 }
 
 Return<Status> Component::reset() {