cfef2714b3f15147b4433b923d5e2fc62e394ef8
jiabin <jiabin@google.com>
2024-10-29 20:52:28 +0000

    audioflinger: align track dump header and content.
    Bug: 376301650
    Test: dumpsys audioflinger
    Flag: EXEMPT log only update
    Change-Id: I63bcfe8a55aeff5ef2559b69a5908a253f936c86

--- services/audioflinger/Tracks.cpp
+++ services/audioflinger/Tracks.cpp
@@ -1090,7 +1090,7 @@ void Track::appendDump(String8& result, bool active) const
     result.appendFormat("%7s %7u/%7u %7u %7u %2s 0x%03X "
                         "%08X %08X %6u "
                         "%2u %3x %2x "
-                        "%5.2g %5.2g %5.2g %5.2g%c %11.2g %12s"
+                        "%5.2g %5.2g %5.2g %5.2g%c %11.2g %10s "
                         "%08X %6zu%c %6zu %c %9u%c %7u %10s %12s",
             active ? "yes" : "no",
             mClient ? mClient->pid() : getpid() ,
