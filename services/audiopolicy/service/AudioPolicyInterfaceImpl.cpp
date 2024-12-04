500b67f48ab228dd1c80259567c9cb59f651d4ab
Jiabin Huang <jiabin@google.com>
2024-12-04 17:56:43 +0000

    Check CAPTURE_MEDIA_OUTPUT permission when it is required.
    Bug: 382270587
    Change-Id: I7d3c13bda55fce8f0e4396e6ee93aa3d0150f0fa
    Test: TH
    Flag: EXEMPT bugfix

--- services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
+++ services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
@@ -2015,7 +2015,7 @@ Status AudioPolicyService::registerPolicyMixes(const std::vector<media::AudioMix
 
     if (needCaptureMediaOutput && !(audioserver_permissions() ?
                 CHECK_PERM(CAPTURE_MEDIA_OUTPUT, attributionSource.uid)
-                : modifyAudioRoutingAllowed())) {
+                : captureMediaOutputAllowed(attributionSource))) {
         return binderStatusFromStatusT(PERMISSION_DENIED);
     }
 
